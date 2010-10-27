5d671718d0d32574080afe70f3b39fef71966c4e
Dan Walkes <danwalkes@gmail.com>
2010-10-26 22:56:00 -0600

    Re-work WakeMeSkiTest and add summary
    Add summary per server of test results.  Don't error out of
    junit test until all tests have completed.

--- src/com/android/wakemeski/test/WakeMeSkiTest.java
+++ src/com/android/wakemeski/test/WakeMeSkiTest.java
@@ -1,5 +1,7 @@
 package com.android.wakemeski.test;
 
+import java.util.ArrayList;
+
 import junit.framework.Assert;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -20,41 +22,115 @@ import com.android.wakemeski.core.WakeMeSkiServer;
  */
 public class WakeMeSkiTest extends AndroidTestCase {
 
-	private int mIndex = 0;
+	private int mServerIndex = 0;
 	private WakeMeSkiServer mServer;
 	private LocationFinder	mFinder;
-	private static final String TAG = "LocationFinderTest";
+	private static final String TAG = "WakeMeSkiTest";
+	private static final int mMaxServers = HttpUtils.SERVER_LIST.length;
+	private ArrayList<String>[] mErrors = ( ArrayList<String>[]) new ArrayList[mMaxServers];
+	private ArrayList<String>[] mFreshSnowNotFound = ( ArrayList<String>[]) new ArrayList[mMaxServers];
+	private ArrayList<String>[] mSuccessLocations = ( ArrayList<String>[])new ArrayList[mMaxServers];
 	
-	/**
-	 * @return the location finder for the next server in the list of supported servers
-	 */
-	LocationFinder nextServer() {
-		if( mIndex < HttpUtils.SERVER_LIST.length ) {
-			String server = HttpUtils.SERVER_LIST[mIndex];
-			Log.i(TAG, "Testing server" + server );
+	protected void setUp() {
+		initServer(0);
+	}
+	
+	protected void tearDown() {
+		String SUMMARY_TAG = new String("WakeMeSkiTest:Summary");
+		int errors = 0;
+		for( int i=0; i<mMaxServers ;i++ ) {
+			if(mErrors[i] != null && mErrors[i].size() != 0 ) {
+				Log.e(SUMMARY_TAG,"Expected no errors running test on server " +
+					HttpUtils.SERVER_LIST[i] + " but " + mErrors[i].size() + " errors occurred:");
+				for( String error : mErrors[i]) {
+					Log.e(SUMMARY_TAG,error);
+				}
+				errors += mErrors[i].size();
+			}
+			if( mFreshSnowNotFound[i] != null &&
+					mFreshSnowNotFound[i].size() != 0 ) {
+				Log.e(SUMMARY_TAG,"Total of " +mFreshSnowNotFound[i].size() + " resorts missing fresh snow totals on server " + HttpUtils.SERVER_LIST[i] + " despite lack of error conditions");
+				
+				for( String location : mFreshSnowNotFound[i]) {
+					Log.e(SUMMARY_TAG,location);
+				}
+			}
+			if( mSuccessLocations[i] != null && 
+					mSuccessLocations[i].size() != 0 ) {
+				Log.i(SUMMARY_TAG,"Total of " + mSuccessLocations[i].size()+ " resorts with no problems found on server " + HttpUtils.SERVER_LIST[i]);
+				for( String location : mSuccessLocations[i]) {
+					Log.i(SUMMARY_TAG,location);
+				}
+			}
+		}
 
+		Assert.assertEquals(0,errors);
+	}
+	
+	boolean initServer(int index) {
+		boolean initSuccess = false;
+		if( index < HttpUtils.SERVER_LIST.length ) {
+			mServerIndex = index;
+			String server = HttpUtils.SERVER_LIST[mServerIndex];
+			Log.i(TAG, "Testing server" + server );
+			mErrors[index] = new ArrayList<String>();
+			mFreshSnowNotFound[index] = new ArrayList<String>();
+			mSuccessLocations[index] = new ArrayList<String>();
+	
 			mServer = new WakeMeSkiServer(server); 
 			mFinder = new LocationFinder(
 					mServer);
-			
-			mIndex++;
-		} else {
-			mFinder = null;
+			initSuccess = true;
 		}
-		return mFinder;
+		return initSuccess;
+	}
+
+	/**
+	 * @return true if a new server is setup for test
+	 */
+	boolean nextServer() {
+		return initServer(mServerIndex+1);
 	}
 	
+	private void expectFalse( boolean condition, String errorIfTrue )
+	{
+		if( condition ) {
+			Log.e(TAG, errorIfTrue);
+			mErrors[mServerIndex].add(errorIfTrue);
+		}
+	}
+	
+	private void expectTrue( boolean condition, String errorIfFalse )
+	{
+		expectFalse(!condition,errorIfFalse);
+	}
+	
+	private void expectEquals( int expected, int actual, String errorIfNoMatch )
+	{
+		expectTrue(expected==actual,errorIfNoMatch + " Expected " + expected + " but found " + actual );
+	}
 
 	/**
 	 * 
 	 * @param location
 	 */
 	private void testLocation(Location location) {
+		boolean hasProblems;
 		Log.i(TAG,"Testing location " + location);
 		Resort resort = new Resort(location);
 		Report r = Report.loadReport(getContext(), 
 				(ConnectivityManager)getContext().getSystemService((Context.CONNECTIVITY_SERVICE)), resort, mServer);
-		Assert.assertFalse(r.hasErrors());
+		hasProblems = r.hasErrors();
+		expectFalse(r.hasErrors(),location + " has errors " + r.getError());
+		if( !hasProblems ) {
+			if( !r.hasFreshSnowTotal() ) {
+				mFreshSnowNotFound[mServerIndex].add(location.toString());
+				hasProblems = true;
+			}
+		}
+		if( !hasProblems ) {
+			mSuccessLocations[mServerIndex].add(location.toString());
+		}
 		/**
 		 * TODO: more testing of this location
 		 */
@@ -62,7 +138,7 @@ public class WakeMeSkiTest extends AndroidTestCase {
 	
 	private void testRegion(String region) throws Throwable {
 		Location locations[] = mFinder.getLocations(region);
-		Assert.assertTrue(locations.length > 0);
+		expectTrue(locations.length > 0,"Expect at least 1 location per region but found 0 in region " + region);
 		Log.i(TAG, "Found " + locations.length + " locations in region " + region);
 		for (Location location:locations) {
 			testLocation(location);
@@ -70,14 +146,14 @@ public class WakeMeSkiTest extends AndroidTestCase {
 	}
 
 	public void testLocations() throws Throwable {
-		LocationFinder finder;
-		while ((finder = nextServer()) != null) {
-			String[] regions = finder.getRegions(); 
-			Assert.assertEquals(9,regions.length);
+		do
+		{
+			String[] regions = mFinder.getRegions(); 
+			expectEquals(9,regions.length,"regions.length did not match expected value");
 			for (String region:regions) {
 				testRegion(region);
 			}
-			
-		}
+		} while (nextServer());
 	}
+	
 }
